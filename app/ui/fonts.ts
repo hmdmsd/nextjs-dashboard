423cbe4cbdd264eae1dfdf5addb9c297d8fabd41
HAMDI Messaoud <messaoud.hamdi@imt-atlantique.net>
2023-12-23 00:04:46 +0100

    font problem

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
